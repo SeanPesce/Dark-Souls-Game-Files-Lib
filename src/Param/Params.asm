5d6e2ef42d738ee83f02b7e4eb746c82ff4a6c2d
NovaFlame <metachrono2@gmail.com>
2018-06-25 19:10:16 -0700

    fix so the asm actually compiles and links

--- src/Param/Params.asm
+++ src/Param/Params.asm
@@ -11,45 +11,45 @@ extern loaded_count_ptr: qword
 PUBLIC asm_param_load_check
 asm_param_load_check PROC
 
-push ebx
-push ecx
-push edx
-push esi
+push rbx
+push rcx
+push rdx
+push rsi
 
-mov ebx, 0 # Index
-mov esi, 0 # Offset
-mov ecx, files_buff_ptr
-mov edx, loaded_count_ptr
-mov edx, [edx]
-cmp edx, ebx
+mov rbx, 0 ; Index
+mov rsi, 0 ; Offset
+mov rcx, files_buff_ptr
+mov rdx, loaded_count_ptr
+mov rdx, [rdx]
+cmp rdx, rbx
 je loop_end
 
-loop_start: # Loop until first unused array element is found
-inc ebx
-add esi, 4
-cmp edx, ebx
+loop_start: ; Loop until first unused array element is found
+inc rbx
+add rsi, 4
+cmp rdx, rbx
 jne loop_start
 
 loop_end:
-# esi now holds offset to write in buffer
-add ecx, esi
-mov [ecx], eax # Store starting address of current param file
+; rsi now holds offset to write in buffer
+add rcx, rsi
+mov [rcx], rax ; Store starting address of current param file
 
-# Increment loaded param file count
-mov ecx, loaded_count_ptr
-mov ebx, dword ptr[ecx]
-add ebx, 1
-mov dword ptr[ecx], ebx
+; Increment loaded param file count
+mov rcx, loaded_count_ptr
+mov rbx, qword ptr[rcx]
+add rbx, 1
+mov qword ptr[rcx], rbx
 
-pop esi
-pop edx
-pop ecx
-pop ebx
+pop rsi
+pop rdx
+pop rcx
+pop rbx
 
-# Original code:
+; Original code:
 mov     [rsp+38h], rbx
 lea     rcx, [rsp+38h]
-call    qword ptr cs:[14051E160h]
+call    qword ptr [14051E160h]
 jmp param_load_asm_return
 
 asm_param_load_check ENDP
